061829caf7dae5bf4f0023255c6b51f6df120acc
max-vac <maxvaclavik@gmail.com>
2020-11-14 17:48:12 -0800

    Completed Rotated Digits

--- rotatedDigits.js
+++ rotatedDigits.js
@@ -1,3 +1,5 @@
+//788. Rotated Digits
+
 // X is a good number if after rotating each digit individually by 180 degrees, we get a valid number that is different from X.  Each digit must be rotated - we cannot choose to leave it alone.
 
 // A number is valid if each digit remains a digit after rotation. 0, 1, and 8 rotate to themselves; 2 and 5 rotate to each other (on this case they are rotated in a different direction, in other words 2 or 5 gets mirrored); 6 and 9 rotate to each other, and the rest of the numbers do not rotate to any other number and become invalid.
@@ -12,24 +14,45 @@
 // Note that 1 and 10 are not good numbers, since they remain unchanged after rotating.
 
 var rotatedDigits = function(N) {
-  //create counter variable set to 0
-  //create obj with keys set to numbers that rotate and their value
+  let counter = 0;
+  let rotatedObj = {
+    1: 1,
+    2: 5,
+    5: 2,
+    6: 9,
+    8: 8,
+    9: 6,
+    0: 0
+  }
 
-  //create function that takes in the N as an argurment
-    //create result var set to empty array
-    //if n === 1
-      //return
+  let checkNumber = (n) => {
+    let result = [];
 
-    //split n into an array
-    //iterate over array
-      //if value is propert in obj
-        //push property value aka the reversed value to the array
+    if (n === 1) {
+      return;
+    }
 
-      //if array joined is not equal to N
-        //increment counter
+    nSplit = n.toString().split('');
 
-    //call function with n -1
+    for (let i = 0; i < nSplit.length; i++) {
+      if (rotatedObj.hasOwnProperty(nSplit[i])) {
+        result.push(rotatedObj[nSplit[i]]);
+      } else if (!rotatedObj.hasOwnProperty(nSplit[i])) {
+        break;
+      } else {
+        result.push(nSplit[i]);
+      }
+    }
 
-  //invoke function with n - 1
-  //return counter
+    if (result.length === nSplit.length && Number(result.join('')) !== n) {
+      counter++;
+    }
+    checkNumber(n - 1);
+  }
+  checkNumber(N);
+  return counter;
 };
+
+// Success
+// Runtime: 120 ms, faster than 10.49% of JavaScript online submissions for Rotated Digits.
+// Memory Usage: 45.6 MB, less than 10.49% of JavaScript online submissions for Rotated Digits.
