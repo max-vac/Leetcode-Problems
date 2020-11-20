1b209f5bf581cff90b98cea2114fb4c956255b91
max-vac <maxvaclavik@gmail.com>
2020-11-20 14:29:36 -0800

    Completed pseudo code for sliding window technique

--- LongestSubstringWithoutRepeatingChars.js
+++ LongestSubstringWithoutRepeatingChars.js
@@ -27,5 +27,21 @@
 // Output: 0
 
 var lengthOfLongestSubstring = function(s) {
+  //create a start varaible set to 0
+  //create longest variable set to 0
+  //create obj varaible set to an empty {}
 
+  //iterate over s
+    //if value not in object
+      //set value key to 0
+
+    //increment the property at that value
+
+    //if the values of the obj are greater than 1
+      //decrement the value at start by 1
+      //increment start
+
+    //longest wil be the result of math max between longest and i - start + 1 for initial start
+
+  //return longest
 };
