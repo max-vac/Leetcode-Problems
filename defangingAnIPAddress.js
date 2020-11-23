6ee15fc6ca92be6dfa3a5627a1825865a625d3a1
max-vac <maxvaclavik@gmail.com>
2020-11-23 14:16:31 -0800

    Completed pseudo code for defangingAnIPAdress.js

---
+++ defangingAnIPAddress.js
@@ -0,0 +1,27 @@
+// 1108. Defanging an IP Address
+// 11/23/2020
+
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+// A defanged IP address replaces every period "." with "[.]".
+
+// Example 1:
+
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+// Example 2:
+
+// Input: address = "255.100.50.0"
+// Output: "255[.]100[.]50[.]0"
+
+var defangIPaddr = function(address) {
+    //create result variable set to an empty string
+
+    //iterate through the address
+      //check to see if the value is equal to a period
+        //result plus a [ then the value then a ]
+      //else
+        //add the value
+
+    //return the result variable
+};
