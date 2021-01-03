3ad455b9368aaf043fffe3fcef7b27043d7f6ae2
max-vac <maxvaclavik@gmail.com>
2021-01-02 18:55:38 -0600

    Completed pseudo code for keyboardRow.js

--- keyboardRow.js
+++ keyboardRow.js
@@ -6,5 +6,18 @@ Input: ["Hello", "Alaska", "Dad", "Peace"]
 Output: ["Alaska", "Dad"]
 
 var findWords = function(words) {
+  //create obj with letters set to the values of their row example qwer=1 asdf=2 zxcv=3
+  //create matchedWords variable set to empty [];
 
+  //iterate over the words input array
+    //create lowerWord to words index set to toLowerCase()
+    //create sum variable
+
+    //iterate over lowerWord
+      //add each value to sum
+
+      // if index is equal to last index && sum === length of word * the value at the key
+        //push that word onto the matchedWords array
+
+  //return the matchedWord array
 };
