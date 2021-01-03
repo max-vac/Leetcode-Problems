894c6402796b5a4aab7f6cc96fdde7546ec47386
max-vac <maxvaclavik@gmail.com>
2021-01-02 18:57:26 -0600

    Completed keyboardRow.js

--- keyboardRow.js
+++ keyboardRow.js
@@ -6,18 +6,26 @@ Input: ["Hello", "Alaska", "Dad", "Peace"]
 Output: ["Alaska", "Dad"]
 
 var findWords = function(words) {
-  //create obj with letters set to the values of their row example qwer=1 asdf=2 zxcv=3
-  //create matchedWords variable set to empty [];
+  let key = {
+    q: 1, w: 1, e: 1, r: 1, t: 1,y: 1,u: 1,i: 1,o: 1,p: 1, a: 2,s: 2,d: 2,f: 2,g: 2,h: 2,j: 2,k: 2,l: 2,z: 3,x: 3,c: 3,v: 3,b: 3,n: 3,m: 3
+  };
+  let matchedWords = [];
 
-  //iterate over the words input array
-    //create lowerWord to words index set to toLowerCase()
-    //create sum variable
+  for (let i = 0; i < words.length; i++) {
+    let lowerCaseWord = words[i].toLowerCase();
+    let sum = 0;
 
-    //iterate over lowerWord
-      //add each value to sum
-
-      // if index is equal to last index && sum === length of word * the value at the key
-        //push that word onto the matchedWords array
-
-  //return the matchedWord array
+    for (let j = 0; j < lowerCaseWord.length; j++) {
+      sum += key[lowerCaseWord[j]];
+      if (j === lowerCaseWord.length - 1 && sum === lowerCaseWord.length * key[lowerCaseWord[j]]) {
+        matchedWords.push(words[i])
+      }
+    }
+  }
+  return matchedWords
 };
+
+// Success
+// Details
+// Runtime: 68 ms, faster than 97.27% of JavaScript online submissions for Keyboard Row.
+// Memory Usage: 38.7 MB, less than 9.29% of JavaScript online submissions for Keyboard Row.
