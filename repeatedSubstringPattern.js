dca26cedf85f0cc9f676a60ce991034014216d0f
max-vac <maxvaclavik@gmail.com>
2021-01-17 18:05:39 -0800

    Completed pseudo code for repeatedSubstringPattern.js

--- repeatedSubstringPattern.js
+++ repeatedSubstringPattern.js
@@ -18,5 +18,21 @@
 // Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
 
 var repeatedSubstringPattern = function(s) {
+  //create variable lps set to length of s
+  //set the first index (0) to 0
+  //create i variable set to 1;
+  //create len variable set to 0
 
+  //while i is less than length of s
+    //if s[i] is equal to sp[len]
+      //increment len
+      //set the value of lps at i to len
+      //increment i
+    //else
+      //if len
+        //set len to the value of lps at len - 1
+      //else
+        //set lps at i to 0
+        //increment i
+    //return whether there is a len the s length % by the last index of the array is equal to 0
 };
