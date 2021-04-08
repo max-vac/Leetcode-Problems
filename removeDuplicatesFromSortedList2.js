83047eef7ccd15ff3022a5845c96c3f3640f0f97
max-vac <maxvaclavik@gmail.com>
2021-04-08 00:58:35 -0700

    Created and completed pseudo code for removeDuplicatesFromSortedList2.js

---
+++ removeDuplicatesFromSortedList2.js
@@ -0,0 +1,28 @@
+// 82. Remove Duplicates from Sorted List II
+// Medium
+
+// Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+// Example 1:
+
+// Input: head = [1,2,3,3,4,4,5]
+// Output: [1,2,5]
+// Example 2:
+
+// Input: head = [1,1,1,2,3]
+// Output: [2,3]
+
+var deleteDuplicates = function (head) {
+  //create sentinel
+  //create previousNode set to the sentinel
+  //create currentNode set to the head
+  //while currentNode
+  //if currentNode val is equal to the currentNode.next val
+  //while currentNode.val is equal to currentNode.next
+  //set currentNode to currentNode.next
+  //set previousNode.next to currentNode.next.next
+  //else
+  //set previous to previous.next
+  //currentNode = currentNode.next
+  //return sentinel.next
+};
