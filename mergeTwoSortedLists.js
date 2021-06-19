9695fa534a80fefc66db34c89be08fc1a1e54537
max-vac <maxvaclavik@gmail.com>
2021-06-18 19:11:58 -0700

    Completed pseudo code for mergeTwoSortedLists.js

--- mergeTwoSortedLists.js
+++ mergeTwoSortedLists.js
@@ -15,4 +15,20 @@
 // Input: l1 = [], l2 = [0]
 // Output: [0]
 
-var mergeTwoLists = function (l1, l2) {};
+var mergeTwoLists = function (l1, l2) {
+  //create variable mergedList set to a new NodeList
+  //create head variable set to the mergedList for later use
+  //while there is a l1 and a l2
+  //if l1 val is less than l2 val
+  //set mergedList.next equal to l1
+  //set l1 to the next node in the l1 list
+  //else
+  //set mergedList.next to l2
+  //set l2 to the next node in the l2 list
+  //set mergedList to the next node of the mergedList
+  //if l1 is null
+  //mergedList.next will be the remainder of l2
+  //if l2 is null
+  //mergeList.next will be the remainder of l1
+  //return the variable head.next so that you remove the initial empty node
+};
